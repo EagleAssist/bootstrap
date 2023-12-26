ba20e805dd024859873f58d7f9398a9ccbbdeb5a
EagleAssist <mamathand98@gmail.com>
2023-12-26 10:33:13 +0530

    change  commit

--- login/models.py
+++ login/models.py
@@ -1,3 +1,29 @@
+# # from django.db import models
+# # from django.contrib.auth.models import AbstractUser
+
+# # class CustomUser(AbstractUser):
+# #     # Add any additional fields you need
+
+# #     # Add related_name to avoid clashes with auth.User model
+# #     groups = models.ManyToManyField(
+# #         'auth.Group',
+# #         related_name='customuser_set',
+# #         related_query_name='user',
+# #         blank=True,
+# #         help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+# #     )
+# #     user_permissions = models.ManyToManyField(
+# #         'auth.Permission',
+# #         related_name='customuser_set',
+# #         related_query_name='user',
+# #         blank=True,
+# #         help_text='Specific permissions for this user.',
+# #     )
+# from django.db import models
+
+# class  CustomUser(models.Model):
+#     username = models.CharField(max_length=255)
+#     password = models.CharField(max_length=255)
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
--- login/views.py
+++ login/views.py
@@ -5,6 +5,10 @@ from rest_framework.views import APIView
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login  
+from django.contrib.auth.hashers import check_password
+from .models import CustomUser as User
+from django.shortcuts import redirect
 
 class LoginView(APIView):
     # authentication_classes = [SessionAuthentication]
@@ -14,16 +18,24 @@ class LoginView(APIView):
         return render(request, 'login.html')
 
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
+        serializer = UserSerializer(data=request.data)        
+        # if serializer.is_valid():
             # Perform authentication logic here (e.g., check username and password)
             # For simplicity, you can use Django's built-in authentication
-            username = serializer.validated_data['username']
-            password = serializer.validated_data['password']
-            user = authenticate(request, username=username, password=password)
+        username = serializer.initial_data['username']
+        password = serializer.initial_data['password']
+            # user = authenticate(request, username=username, password=password)
+            # Check if the user with the provided username exists
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({'message': 'Invalid login credentials'}, status=401)
+        
+        
+        if password == user.password:
+            login(request, user)
+            return redirect('/table/modellist/')
+        else:
+            return Response({'message': 'Invalid Login credentials'}, status=401)
 
-            if user:
-                login(request, user)
-                return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
